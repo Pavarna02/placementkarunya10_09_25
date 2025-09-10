56ba4f5b9bf127f49d455da42fa7c851d90f96be
Pavarna <153485403+Pavarna02@users.noreply.github.com>
2025-09-10 16:13:24 +0530

    Create C-L0-MOD3-Challenge9.java

---
+++ C-L0-MOD3-Challenge9.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();  
+        int b = sc.nextInt();  
+
+        int result = 0;
+        int i = 0;
+
+        while (i < b) {
+            result = result + a;
+            i++;
+        }
+
+        System.out.println(result);
+    }
+}
